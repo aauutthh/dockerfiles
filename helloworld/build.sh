4684d6707e7c9bdfcb0cd62e2bf8245e5673a0f3
aauutthh <daniel@kldebian>
2018-08-03 15:34:17 +0800

    save

---
+++ helloworld/build.sh
@@ -0,0 +1,27 @@
+build_and_test()
+{
+    local tagname=$1
+    shift
+    docker build -t $tagname $@ --no-cache .
+    docker images
+    docker run -it --rm $tagname
+    docker image rm $tagname
+}
+
+build_noargs()
+{
+    # 不传递--build-arg ,则镜像默认输出hello
+    local tagname=helloworld/helloworld:noargs
+    build_and_test $tagname 
+}
+
+build_with_args()
+{
+    # 传递--build-arg ,则镜像输出world
+    local tagname=helloworld/helloworld:hasargs
+    build_and_test $tagname --build-arg passarg="world" 
+}
+
+build_noargs
+build_with_args
+
